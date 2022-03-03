284becfc5b223bb240bb6a5af947ee98dc773e29
Danni <dannielle.young@testdrivensolutions.co.uk>
2022-03-03 15:28:21 +0000

    started front end and added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+type Event = {
+  name: string;
+  date: string;
+}
 
 function App() {
+  const [eventName, setEventName] = useState<string>('')
+  const [date, setDate] = useState<string>('')
+  const [upcomingEvents, setUpcomingEvents] = useState<Event[]>([])
+  console.log(upcomingEvents)
+  const formSubmit = (e: any) => {
+    e.preventDefault()
+    if (eventName.length !== 0 && date.length !== 0) {
+      const updatedEvents = [...upcomingEvents, { name: eventName, date }]
+      setUpcomingEvents(updatedEvents)
+      setDate('')
+      setEventName('')
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-col mx-auto w-1/2'>
+      <h1 className='text-xl'>Countdown app</h1>
+      <p>Got an exciting day coming up? Let's start a countdown!</p>
+      <form onSubmit={formSubmit}>
+
+        <label>Name:
+          <input className='border' name='Name' type='text' value={eventName} onChange={(event) => setEventName(event.target.value)} />
+        </label>
+        <label>Date:
+          <input className='border' name='Date' type='date' value={date} onChange={(event) => setDate(event.target.value)} />
+        </label>
+        <button type='submit' className='bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded w-2/3'>Add event to see the countdown</button>
+      </form>
+
+      {upcomingEvents.map((event: Event) => {
+        return (<div>
+          <p>{event.name} happening {event.date}</p>
+
+        </div>)
+
+      })}
     </div>
   );
 }
