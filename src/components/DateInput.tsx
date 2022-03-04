107faf5d7e683aaa4ac10bb020ab23946385e1da
Danni <dannielle.young@testdrivensolutions.co.uk>
2022-03-04 13:37:23 +0000

    added break

--- src/components/DateInput.tsx
+++ src/components/DateInput.tsx
@@ -17,6 +17,7 @@ const DateInput = (props: Props) => {
             <label>Date:
                 <input className='border mt-2 ml-3' name='Date' type='date' value={date} onChange={(event) => setDate(event.target.value)} />
             </label>
+            <br></br>
             <button type='submit' className='bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded w-2/3 mt-2'>Add event to see the countdown</button>
         </form>
     );
